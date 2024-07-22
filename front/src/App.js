fdd3be52d3bb772cd491f52dce7beb67672de279
Francis Lovo <francis.carolinalovo@gmail.com>
2024-07-21 21:38:10 -0300

    router e header

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Formulario from "./componentes/Formulario";
+import Header from "./componentes/Header";
 
 function App() {
   return (
     <div className="App">
-      <Formulario />
+      <BrowserRouter>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="login" element={<Formulario />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ front/src/componentes/Header/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Header.css";
+
+import { Link } from "react-router-dom";
+
+const Header = (props) => {
+  return (
+    <nav className="navbar">
+      <div className="logo">MeuLogo</div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/sobre">Sobre</Link>
+        </li>
+        <li>
+          <Link to="/contato">Contato</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
