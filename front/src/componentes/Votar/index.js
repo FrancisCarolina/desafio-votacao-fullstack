a83019f481479811b0ce38db78cbf893c493f1a8
Francis Lovo <francis.carolinalovo@gmail.com>
2024-07-23 20:40:39 -0300

    carregando votacao a partir do codigo

--- front/src/componentes/Votar/index.js
+++ front/src/componentes/Votar/index.js
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Votar.css";
 import Botao from "../Botao";
 import CampoSelecionar from "../CampoSelecionar";
 import Tempo from "../Tempo";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Loader from "../Loader";
+import axios from "axios";
+import Modal from "../Modal";
 
 const Votar = (props) => {
-  const date = new Date(2024, 6, 23, 12, 44, 0); // 23 de julho de 2024, meia-noite
-  const tempoDuracao = "1:10";
-  const [pauta] = useState("Você gosta de gatos?");
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [textoModal, setTextoModal] = useState("");
+  const [tituloModal, setTituloModal] = useState("");
+  const [pauta, setPauta] = useState();
   const [selecionado, setSelecionado] = useState("");
   const [tempoAcabou, setTempoAcabou] = useState(false);
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    if (tituloModal === "Falha!") {
+      navigate("/votar");
+    }
+  };
 
   const aoAlterado = (valor) => {
     setSelecionado(valor);
@@ -31,10 +51,43 @@ const Votar = (props) => {
     }
   };
 
+  useEffect(() => {
+    const buscarPautaPorId = async (id) => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          `http://localhost:3003/pauta?codigo=${id}`
+        );
+        const data = response.data;
+        if (data.length > 0) {
+          console.log("PAUTA: ", data[0]);
+          setPauta(data[0]);
+        }
+      } catch (err) {
+        console.log("Erro GET Associado: ", err);
+        setTextoModal("Não foi possível encontrar uma votação com esse código");
+        setTituloModal("Falha!");
+        handleOpenModal();
+      }
+      setLoading(false);
+    };
+    if (id) {
+      buscarPautaPorId(id);
+    }
+  }, [id]);
+
   return (
     <div className="votar">
+      <Loader show={loading} />
+      <Modal
+        show={showModal}
+        onClose={handleCloseModal}
+        onOk={handleCloseModal}
+        texto={textoModal}
+        titulo={tituloModal}
+      />
       <form onSubmit={aoSubmeter}>
-        <span>{pauta}</span>
+        {!!pauta && !!pauta.pergunta && <span>{pauta.pergunta}</span>}
         <CampoSelecionar
           label={"Sim"}
           valor={"sim"}
@@ -48,11 +101,13 @@ const Votar = (props) => {
           aoAlterado={(valor) => aoAlterado(valor)}
           selectedOption={selecionado}
         />
-        <Tempo
-          date={date}
-          tempoDuracao={tempoDuracao}
-          aoMudarTempo={(tempoRestante) => aoMudarTempo(tempoRestante)}
-        />
+        {!!pauta && !!pauta.iniciadoEm && !!pauta.duracao && (
+          <Tempo
+            date={pauta.iniciadoEm}
+            tempoDuracao={pauta.duracao}
+            aoMudarTempo={(tempoRestante) => aoMudarTempo(tempoRestante)}
+          />
+        )}
         <div>
           <Botao disabled={tempoAcabou}>Enviar Resposta</Botao>
         </div>
