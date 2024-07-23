3feb55d5b147f9cf74c0fb6afd69aa78f95a4e76
Francis Lovo <francis.carolinalovo@gmail.com>
2024-07-23 19:47:14 -0300

    carregando minhas votacoes

--- front/src/componentes/CardVotacao/index.js
+++ front/src/componentes/CardVotacao/index.js
@@ -2,13 +2,24 @@ import React from "react";
 import "./CardVotacao.css";
 
 const CardVotacao = (props) => {
+  const formatarData = (date) => {
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+  };
   return (
     <section className="card-votacao" onClick={props.onClick}>
       <span className="titulo">{props.pauta}</span>
       <div className="conteudo">
         <div className="tempo">
           <span>Iniciada em: </span>
-          <span>{props.iniciada}</span>
+          <span>{formatarData(props.iniciada)}</span>
         </div>
         <div className="tempo">
           <span>Duração: </span>
--- front/src/componentes/MinhasVotacoes/index.js
+++ front/src/componentes/MinhasVotacoes/index.js
@@ -1,36 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./MinhasVotacoes.css";
 import Botao from "../Botao";
 import CardVotacao from "../CardVotacao";
 import { useNavigate } from "react-router-dom";
+import Loader from "../Loader";
+import axios from "axios";
+import Modal from "../Modal";
 
 const MinhasVotacoes = () => {
   const navigate = useNavigate();
-  const minhasVotacoes = [
-    { id: 1, pauta: "Pauta 1", duracao: "20:00", iniciada: "22:00" },
-    { id: 2, pauta: "Pauta 2", duracao: "30:00", iniciada: "21:00" },
-    { id: 3, pauta: "Pauta 3", duracao: "00:30", iniciada: "20:00" },
-  ];
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [textoModal, setTextoModal] = useState("");
+  const [tituloModal, setTituloModal] = useState("");
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    if (tituloModal === "Falha!") {
+      navigate("/");
+    }
+  };
+
+  const [minhasVotacoes, setMinhasVotacoes] = useState();
   const aoClicar = () => {
     navigate("/novaVotacao");
   };
+
+  const stringParaDate = (string) => {
+    return new Date(string);
+  };
+
+  useEffect(() => {
+    const idAssociado = 1;
+    const buscarPautasPorAssociado = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          `http://localhost:3003/pauta?idAssociado=${idAssociado}`
+        );
+        const data = response.data;
+        if (data.length > 0) {
+          //return true;
+          setMinhasVotacoes(data);
+        }
+      } catch (err) {
+        console.log("Erro GET Associado: ", err);
+        setTextoModal("A operação falhou");
+        setTituloModal("Falha!");
+        handleOpenModal();
+      }
+      setLoading(false);
+    };
+    buscarPautasPorAssociado();
+  }, []);
   return (
     <section className="minhas-votacoes">
+      <Loader show={loading} />
+      <Modal
+        show={showModal}
+        onClose={handleCloseModal}
+        onOk={handleCloseModal}
+        texto={textoModal}
+        titulo={tituloModal}
+      />
       <div>
         <Botao onClick={aoClicar}>Nova Votação</Botao>
       </div>
       <div className="lista-votacoes">
-        {minhasVotacoes.map((votacao) => (
-          <CardVotacao
-            key={votacao.id}
-            pauta={votacao.pauta}
-            duracao={votacao.duracao}
-            iniciada={votacao.iniciada}
-            onClick={() => {
-              navigate(`/minhasVotacoes/${votacao.id}`);
-            }}
-          />
-        ))}
+        {!!minhasVotacoes &&
+          minhasVotacoes.length > 0 &&
+          minhasVotacoes.map((votacao) => (
+            <CardVotacao
+              key={votacao.id}
+              pauta={votacao.pergunta}
+              duracao={votacao.duracao}
+              iniciada={stringParaDate(votacao.iniciadoEm)}
+              onClick={() => {
+                navigate(`/minhasVotacoes/${votacao.id}`);
+              }}
+            />
+          ))}
+        {(!minhasVotacoes || minhasVotacoes.length === 0) && (
+          <span>Você ainda não criou uma votação.</span>
+        )}
       </div>
     </section>
   );
