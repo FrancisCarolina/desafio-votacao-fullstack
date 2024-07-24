064a063f0572b11cddfaefdcf7622f4a8c75f392
Francis Lovo <francis.carolinalovo@gmail.com>
2024-07-23 21:39:41 -0300

    criar pauta

--- front/src/componentes/NovaVotacao/index.js
+++ front/src/componentes/NovaVotacao/index.js
@@ -3,12 +3,33 @@ import "./NovaVotacao.css";
 import CampoTexto from "../CampoTexto";
 import Botao from "../Botao";
 import ListaRespostas from "../ListaRespostas";
+import Loader from "../Loader";
+import Modal from "../Modal";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
 
 const NovaVotacao = (props) => {
+  const navigate = useNavigate();
   const [pauta, setPauta] = useState("");
   const [tempo, setTempo] = useState("00:00:00");
   const [erroPauta, setErroPauta] = useState("");
   const [erroTempo, setErroTempo] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [textoModal, setTextoModal] = useState("");
+  const [tituloModal, setTituloModal] = useState("");
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    if (tituloModal === "Sucesso!") {
+      navigate("/");
+    }
+  };
 
   const aoAlteradoTempo = (valor) => {
     setTempo(valor);
@@ -17,7 +38,15 @@ const NovaVotacao = (props) => {
     setPauta(valor);
   };
 
-  const aoSalvar = (evento) => {
+  const geradorCodigo = () => {
+    const uuid = uuidv4();
+
+    const base64UUID = btoa(uuid.replace(/-/g, ""));
+
+    return base64UUID.substring(0, 16);
+  };
+
+  const aoSalvar = async (evento) => {
     evento.preventDefault();
     if (tempo === "00:00:00" || tempo === "--:--:--" || pauta === "") {
       if (pauta === "") {
@@ -27,15 +56,45 @@ const NovaVotacao = (props) => {
         setErroTempo("Campo obrigatório");
       }
     } else {
+      try {
+        const date = new Date();
+        const res = await axios.post("http://localhost:3003/pauta", {
+          iniciadoEm: date.toISOString(),
+          duracao: tempo,
+          pergunta: pauta,
+          codigo: geradorCodigo(),
+          idAssociado: +sessionStorage.getItem("associadoLogado"),
+        });
+
+        setPauta("");
+        setTempo("00:00:00");
+        setTextoModal(
+          "A operação foi concluída com sucesso. ACESSO A PARTIR DO CÓDIGO GERADO: " +
+            res.data.codigo
+        );
+        setTituloModal("Sucesso!");
+      } catch (err) {
+        console.log("Erro POST Pauta: ", err);
+        setTextoModal("A operação falhou");
+        setTituloModal("Falha!");
+      }
+      setLoading(false);
+      handleOpenModal();
       setErroTempo("");
       setErroPauta("");
-      setPauta("");
-      setTempo("00:00:00");
     }
   };
 
   return (
     <div className="nova-votacao">
+      <Loader show={loading} />
+      <Modal
+        show={showModal}
+        onClose={handleCloseModal}
+        onOk={handleCloseModal}
+        texto={textoModal}
+        titulo={tituloModal}
+      />
       <form onSubmit={aoSalvar}>
         <h2>Nova Votação</h2>
         <CampoTexto
